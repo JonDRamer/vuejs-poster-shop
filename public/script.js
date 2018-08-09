070ad227d65c97e3af20da5c3bf27d96e120ac2e
jramer <jramer87@gmail.com>
2018-08-09 14:47:46 -0500

    Adds cart items, order total, and item quantity

--- public/script.js
+++ public/script.js
@@ -1,1 +1,38 @@
-console.log('It works.');
+'use strict';
+
+new Vue({
+    el: '#app',
+    data: {
+        total: 0,
+        items: [
+            { id: 1, title: "Item 1", price: 4.99 },
+            { id: 2, title: "Item 2", price: 3.99 },
+            { id: 3, title: "Item 3", price: 2.99 }
+        ],
+        cart: []
+    },
+    methods: {
+        addItem: function(index) {
+            let item = this.items[index];
+            let found = false;
+
+            this.cart.forEach(( cartItem ) => {
+                if (cartItem.id === item.id) {
+                    found = true;
+                    cartItem.quantity++;
+                }
+            });
+
+            if (!found) {
+                this.cart.push({
+                    id: item.id,
+                    title: item.title,
+                    quantity: 1
+                });
+            };
+
+            this.total += item.price;
+            
+        }
+    }
+});
